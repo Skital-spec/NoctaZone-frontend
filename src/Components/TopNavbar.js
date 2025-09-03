6452ca92f5da8603284f0d9353a2f2c70f602db1
Skital-spec <teddygitonga80@gmail.com>
2025-09-03 18:56:01 +0300

    Set up Daraja callback and base URL in .env and server.js

--- src/Components/TopNavbar.js
+++ src/Components/TopNavbar.js
@@ -38,7 +38,7 @@ const TopNavbar = ({ onOpenPublicChat }) => {
       // console.log("Fetching balance from API for user:", uid);
       
       const response = await fetch(
-        `https://safcom-payment.onrender.com:5000/api/wallet/transaction?user_id=${uid}`
+        `https://safcom-payment.onrender.com/api/wallet/transaction?user_id=${uid}`
       );
       
       if (!response.ok) {
